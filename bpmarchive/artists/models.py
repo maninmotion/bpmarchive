a4552a3ae7a251927bc463f2db34be70c35fc0ba
mainnmotion <kevin@maninmotion.com>
2013-10-29 10:25:33 -0400

    adding artist models and skeleton

--- bpmarchive/artists/models.py
+++ bpmarchive/artists/models.py
@@ -1,1 +1,31 @@
+from django.db import models
+
 __author__ = 'Kevin'
+from django.db import models
+
+
+class Hometown(models.Model):
+    name = models.CharField(max_length=200)
+    state = models.CharField(max_length=200)
+    country = models.CharField(max_length=200)
+
+    def __unicode__(self):
+        return self.name
+
+
+class Genre(models.Model):
+    name = models.CharField(max_length=200)
+
+    def __unicode__(self):
+        return self.name
+
+
+class Artist(models.Model):
+    name = models.CharField(max_length=200)
+    hometown = models.ForeignKey(Hometown)
+    genre = models.ForeignKey(Genre)
+
+    def __unicode__(self):
+        return self.name
+
+

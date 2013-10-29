458cfeedb740aa1df2be103527f662889ef3b614
mainnmotion <kevin@maninmotion.com>
2013-10-29 09:20:49 -0400

    adding artists

--- bpmarchive/bpmarchive/models.py
+++
@@ -1,9 +0,0 @@
-__author__ = 'Kevin'
-from django.db import models
-
-
-class Test(models.Model):
-    test_text = models.CharField(max_length=200)
-
-    def __unicode__(self):
-        return self.test_text
